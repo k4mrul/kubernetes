839885aa727d39a5173041758d5db56b94faf7ae
k4mrul <kamrul@fish.cat>
2021-11-01 08:04:28 +0600

    fix8

--- cluster-provision/lxd/bootstrap-kube-ubuntu.sh
+++ cluster-provision/lxd/bootstrap-kube-ubuntu.sh
@@ -3,22 +3,22 @@
 # This script has been tested on Ubuntu 20.04
 # For other versions of Ubuntu, you might need some tweaking
 
-echo "[TASK 1] Install docker runtime"
+KUBE_VERSION=1.22.2
+
+echo "[TASK 1] Install containerd runtime"
 apt update -qq >/dev/null 2>&1
-apt install -qq -y ca-certificates apt-transport-https lsb-release gnupg >/dev/null 2>&1
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  >/dev/null 2>&1
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
-apt update -qq >/dev/null 2>&1
-apt install -qq -y docker-ce docker-ce-cli containerd.io >/dev/null 2>&1
-systemctl enable docker >/dev/null 2>&1
-systemctl start docker >/dev/null 2>&1
+apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+mkdir /etc/containerd
+containerd config default > /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null 2>&1
 
 echo "[TASK 2] Add apt repo for kubernetes"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
 
 echo "[TASK 3] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-apt install -qq -y kubeadm=1.21.0-00 kubelet=1.21.0-00 kubectl=1.21.0-00 >/dev/null 2>&1
+apt install -qq -y kubeadm=${KUBE_VERSION}-00 kubelet=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 >/dev/null 2>&1
 echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
 systemctl restart kubelet
 
@@ -68,7 +68,7 @@ then
   echo "[TASK 11] Deploy Weave network"
   kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.33.0.0/16" > /dev/null 2>&1
   
-  echo "[TASK 12] Generate and save cluster join command to /joincluster.sh"
+  echo "[TASK 12] Run this join command in worker node"
   joinCommand=$(kubeadm token create --print-join-command 2>/dev/null) 
   echo "$joinCommand --ignore-preflight-errors=all" > /joincluster.sh
 
